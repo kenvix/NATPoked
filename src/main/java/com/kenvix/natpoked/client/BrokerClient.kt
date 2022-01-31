13ca27dd03997220777fbc374df06ae8d014b165
Kenvix Zure <kenvixzure@live.com>
2022-02-01 00:00:17 +0800

    BrokerClient: Init

---
+++ src/main/java/com/kenvix/natpoked/client/BrokerClient.kt
@@ -0,0 +1,12 @@
+//--------------------------------------------------
+// Class BrokerClient
+//--------------------------------------------------
+// Written by Kenvix <i@kenvix.com>
+//--------------------------------------------------
+
+package com.kenvix.natpoked.client
+
+class BrokerClient(
+
+) {
+}
